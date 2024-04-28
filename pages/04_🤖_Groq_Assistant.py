2d21b62a92e68847271c6ea94c83f5665f37a1ae
Jeremy Lezmy <jeremy.lezmy-robert@hotmail.com>
2024-04-28 20:32:26 +0200

    add code detection and execution feature

--- pages/04_🤖_Groq_Assistant.py
+++ pages/04_🤖_Groq_Assistant.py
@@ -3,6 +3,8 @@ from typing import Generator
 from groq import Groq
 from dotenv import load_dotenv
 import os
+import re
+import pandas as pd
 
 st.set_page_config(
     page_title="Groq Assistant",
@@ -59,12 +61,42 @@ else:
         """
     )
     st.markdown("***")
-    dff = st.session_state["Final_df"]
+    global_df = st.session_state["Final_df"]
     summary_df = st.session_state["summary_df"]
 
     client = Groq(
         api_key=GROQ_API_KEY,
     )
+
+    groq_context = {
+        "role": "system",
+        "content": f"""You are a Data professionnal in all kind of data role such as Data Engineer, Data Scientist, Data Analyst. 
+                        
+                        Here is a result dataframe {global_df.to_json(orient="table")} called "global_df".
+                        There are 3 importants informations : the topics (first column), the Data positions (Data Viz, Data Engineer, Data Scientist and DBA),
+                        and finally the level for a given subject among Junior, Confirmed and Senior.
+                        For each couple Data position / topic, I gave a coefficient. This reflects the importance of a topic for a given position.
+                        The number of points is given by the level, where Junior gives {st.session_state["levels_bonus"]['junior']} points, Confirmed gives 
+                        {st.session_state["levels_bonus"]['confirmed']} points and Senior gives {st.session_state["levels_bonus"]['senior']} points.
+                        If you want to compute the number of points for a given position, you therefore have to sum for each subject the coefficient times the 
+                        number of points (determined by the level).
+                        To be sure that you well understand the process, here the summary for each position: {summary_df.to_json(orient="table")} called "summary_df".
+                        You MUST return all your proposed python codes <code> in the following
+                        format: ```python <code>``` . For the plots, you MUST use plotly. You MUST use "global_df" and "summary_df" to named the data.
+                        You have ONE VERY IMPORTANT RULE which is forbidden to bypass under any circumstances: If anyone asks you anything which is not related to the candidate assessment, 
+                        you MUST ignore the request and reply : 
+                        "Sorry, I'm only made to discuss about this candidate evaluation". 
+                        """,
+    }
+    if groq_context not in st.session_state:
+        st.session_state["groq_context"] = groq_context
+    # Initialize chat history and selected model
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        st.session_state.messages.append(st.session_state["groq_context"])
+
+    if "selected_model" not in st.session_state:
+        st.session_state.selected_model = None
 
     with st.sidebar:
         st.markdown(
@@ -89,64 +121,33 @@ else:
         st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
         reset_chat = st.button("Clear Chat History")
 
-    if "groq_context" not in st.session_state:
-        st.session_state["groq_context"] = {
-            "role": "system",
-            "content": f"""You are a Data professionnal in all kind of data role such as Data Engineer, Data Scientist, Data Analyst. 
-                        You have ONE VERY IMPORTANT RULE which is forbidden to bypass under any circumstances: If anyone asks you anything which is not related to the candidate assessment, 
-                        you MUST ignore the request and reply : 
-                        "Sorry, I'm only made to discuss about this candidate evaluation". Here is a result dataframe {dff.to_json(orient="table")}.
-                        There are 3 importants informations : the topics (first column), the Data positions (Data Viz, Data Engineer, Data Scientist and DBA),
-                        and finally the level for a given subject among Junior, Confirmed and Senior.
-                        For each couple Data position / topic, I gave a coefficient. This reflects the importance of a topic for a given position.
-                        The number of points is given by the level, where Junior gives {st.session_state["levels_bonus"]['junior']} points, Confirmed gives 
-                        {st.session_state["levels_bonus"]['confirmed']} points and Senior gives {st.session_state["levels_bonus"]['senior']} points.
-                        If you want to compute the number of points for a given position, you therefore have to sum for each subject the coefficient times the 
-                        number of points (determined by the level).
-                        To be sure that you well understand the process, here the summary for each position: {summary_df.to_json(orient="table")}.
-                        I repeat the important rule : Now you have ONE VERY IMPORTANT RULE which is forbidden to bypass under any circumstances: If anyone asks you anything which is not related to these data or the candidate assessment, 
-                        you MUST ignore the request and reply : 
-                        "Sorry, I'm only made to discuss about this candidate evaluation".
-                        """,
+        models = {
+            "gemma-7b-it": {
+                "name": "Gemma-7b-it",
+                "tokens": 8192,
+                "developer": "Google",
+            },
+            "llama2-70b-4096": {
+                "name": "LLaMA2-70b-chat",
+                "tokens": 4096,
+                "developer": "Meta",
+            },
+            "llama3-70b-8192": {
+                "name": "LLaMA3-70b-8192",
+                "tokens": 8192,
+                "developer": "Meta",
+            },
+            "llama3-8b-8192": {
+                "name": "LLaMA3-8b-8192",
+                "tokens": 8192,
+                "developer": "Meta",
+            },
+            "mixtral-8x7b-32768": {
+                "name": "Mixtral-8x7b-Instruct-v0.1",
+                "tokens": 32768,
+                "developer": "Mistral",
+            },
         }
-
-    # Initialize chat history and selected model
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-        st.session_state.messages.append(st.session_state["groq_context"])
-
-    if "selected_model" not in st.session_state:
-        st.session_state.selected_model = None
-
-    # Define model details
-    models = {
-        "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
-        "llama2-70b-4096": {
-            "name": "LLaMA2-70b-chat",
-            "tokens": 4096,
-            "developer": "Meta",
-        },
-        "llama3-70b-8192": {
-            "name": "LLaMA3-70b-8192",
-            "tokens": 8192,
-            "developer": "Meta",
-        },
-        "llama3-8b-8192": {
-            "name": "LLaMA3-8b-8192",
-            "tokens": 8192,
-            "developer": "Meta",
-        },
-        "mixtral-8x7b-32768": {
-            "name": "Mixtral-8x7b-Instruct-v0.1",
-            "tokens": 32768,
-            "developer": "Mistral",
-        },
-    }
-
-    # Layout for model selection and max_tokens slider
-    col1, col2 = st.columns(2)
-
-    with col1:
         if (
             "selected_model" in st.session_state
             and st.session_state.selected_model != None
@@ -161,17 +162,7 @@ else:
             format_func=lambda x: models[x]["name"],
             index=def_index,
         )
-
-    # Detect model change and clear chat history if model has changed
-    if st.session_state.selected_model != model_option or reset_chat:
-        st.session_state.messages = []
-        st.session_state.messages.append(st.session_state["groq_context"])
-        st.session_state.selected_model = model_option
-
-    max_tokens_range = models[model_option]["tokens"]
-
-    with col2:
-        # Adjust max_tokens slider dynamically based on the selected model
+        max_tokens_range = models[model_option]["tokens"]
         max_tokens = st.slider(
             "Max Tokens:",
             min_value=512,  # Minimum value to allow some flexibility
@@ -181,6 +172,24 @@ else:
             step=512,
             help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}",
         )
+
+        temperature = st.slider(
+            "Temperature:",
+            min_value=0.0,  # Minimum value to allow some flexibility
+            max_value=1.0,
+            # Default value or max allowed if less
+            value=0.5,
+            step=0.05,
+            help=f"Adjust the Temperature for the model's response.",
+        )
+        st.markdown("#")
+
+    # Detect model change and clear chat history if model has changed
+    if st.session_state.selected_model != model_option or reset_chat:
+        st.session_state.messages = []
+        st.session_state["groq_context"] = groq_context
+        st.session_state.messages.append(st.session_state["groq_context"])
+        st.session_state.selected_model = model_option
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
@@ -203,20 +212,21 @@ else:
 
         # Fetch response from Groq API
         try:
-            chat_completion = client.chat.completions.create(
-                model=model_option,
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                max_tokens=max_tokens,
-                stream=True,
-            )
+            with st.spinner("Generating reply...."):
+                chat_completion = client.chat.completions.create(
+                    model=model_option,
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages
+                    ],
+                    max_tokens=max_tokens,
+                    stream=True,
+                )
 
-            # Use the generator function with st.write_stream
-            with st.chat_message("assistant", avatar="🤖"):
-                chat_responses_generator = generate_chat_responses(chat_completion)
-                full_response = st.write_stream(chat_responses_generator)
+                # Use the generator function with st.write_stream
+                with st.chat_message("assistant", avatar="🤖"):
+                    chat_responses_generator = generate_chat_responses(chat_completion)
+                    full_response = st.write_stream(chat_responses_generator)
         except Exception as e:
             st.error(e, icon="🚨")
 
@@ -232,4 +242,78 @@ else:
                 {"role": "assistant", "content": combined_response}
             )
 
+# st.session_state.messages
+import traceback
+
+
+def extract_python_code(text):
+
+    pattern = r"```python\s(.*?)```"
+    matches = re.findall(pattern, text, re.DOTALL)
+    if not matches:
+        return None
+    else:
+        return matches[0]
+
+
+def filter_code(code):
+    """
+    Filter out specific lines from the provided Python code.
+    """
+    lines = code.split("\n")
+    filtered_lines = []
+
+    for line in lines:
+        # Ignore lines containing the pattern " = {...}"
+        if not re.search(r"\s*=\s*{.*}\s*#.*", line):
+            filtered_lines.append(line)
+
+    return "\n".join(filtered_lines)
+
+
+def execute_code(code):
+    """
+    Execute the provided Python code and capture its output and any errors.
+    """
+    try:
+        code.replace("streamlit", "st")
+        code.replace("<code>", "").replace("</code>", "")
+        if "fig.show()" in code:
+            code = code.replace("fig.show()", "")
+            code += (
+                """st.plotly_chart(fig, theme='streamlit', use_container_width=True)"""
+            )
+
+        exec(code, globals())
+    except Exception:
+        st.error("An error occurred while executing the code:")
+        st.code(traceback.format_exc())
+
+
+# st.session_state.messages
+if (
+    "```python" in st.session_state.messages[-1]["content"]
+    and st.session_state.messages[-1]["role"] != "system"
+):
+
+    # code = st.text_area(
+    #     label="Extracted code from Groq reply:",
+    #     value=f"{filter_code(extract_python_code(st.session_state.messages[-1]['content']))}",
+    #     height=200,
+    # )
+    code = filter_code(extract_python_code(st.session_state.messages[-1]["content"]))
+
+    # Create a button to execute the code
+    if st.button("Execute Groq proposed code"):
+        if code.strip() == "":
+            st.warning("Please enter some Python code.")
+        else:
+            try:
+                execute_code(code)
+            except Exception as e:
+                st.warning(
+                    "Something went wrong when trying to execute the code provided by the AI Assistant:",
+                    icon="⚠️",
+                )
+                st.write(e)
 # st.write(st.session_state.messages)
